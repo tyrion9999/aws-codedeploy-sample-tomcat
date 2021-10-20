42507e4b63e221e0924e8cf2b178e5f218e90ea2
tyrion9999 <89721207+tyrion9999@users.noreply.github.com>
2021-10-20 13:44:34 +0530

    Update write_codedeploy_config.sh

--- scripts/write_codedeploy_config.sh
+++ scripts/write_codedeploy_config.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-mkdir -p /var/codedeploy/tomcat-sample
 
-cat <<EOF >/var/codedeploy/tomcat-sample/env.properties
-APPLICATION_NAME=$APPLICATION_NAME
-DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
-DEPLOYMENT_ID=$DEPLOYMENT_ID
-EOF
+# mkdir -p /var/codedeploy/tomcat-sample
+
+# cat <<EOF >/var/codedeploy/tomcat-sample/env.properties
+# APPLICATION_NAME=$APPLICATION_NAME
+# DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
+# DEPLOYMENT_ID=$DEPLOYMENT_ID
+# EOF
